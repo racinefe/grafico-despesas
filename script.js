7095a1266f814e92d6fa29c9d1812b9072c4c5e3
racine fellipe <racine.3301@gmail.com>
2023-06-10 15:20:56 -0300

    adicionando o form au html, criando cosntruoção do crafico linha e alimentando os braficos om os inputs, adicioando reset dos campos

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
+//Aqui o código está realizando uma requisição HTTP para o arquivo "data.json" e, em seguida, processando a resposta em formato JSON.
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
     const dias = data.map((item) => item.day);
-    
-    // Carregar dados do Armazenamento Local ou usar valores padrão
-    let valores = JSON.parse(localStorage.getItem('valores')) || [
-      { "day": "Segunda-feira", "amount": 0 },
-      { "day": "Terça-feira", "amount": 0 },
-      { "day": "Quarta-feira", "amount": 0 },
-      { "day": "Quinta-feira", "amount": 0 },
-      { "day": "Sexta-feira", "amount": 0 },
-      { "day": "Sábado", "amount": 0 },
-      { "day": "Domingo", "amount": 0 }
+
+    // Carregar dados do Armazenamento Local ou usa valores padrão
+    let valores = JSON.parse(localStorage.getItem("valores")) || [
+      { day: "Segunda-feira", amount: 0 },
+      { day: "Terça-feira", amount: 0 },
+      { day: "Quarta-feira", amount: 0 },
+      { day: "Quinta-feira", amount: 0 },
+      { day: "Sexta-feira", amount: 0 },
+      { day: "Sábado", amount: 0 },
+      { day: "Domingo", amount: 0 },
     ];
 
     const ctx = document.getElementById("grafico");
 
+    //Aqui temos a estrutura do objeto passado para a configuração do gráfico
     const chart = new Chart(ctx, {
-      type: "bar",
+      
       data: {
-        labels: dias,
-
-        datasets: [
-          {
-            label: "My balance",
-            backgroundColor: "hsl(10, 79%, 65%, 0.5)",
-            hoverBackgroundColor: "hsl(209, 68%, 66%, 0.3)",
-            data: valores.map(item => item.amount),
+        datasets: [{
+            type: "bar",
+            label: "Gráfico Barra",
+            labels: dias,
+            backgroundColor: "hsl(10, 79%, 65%)",
+            hoverBackgroundColor: "hsl(209, 68%, 66%, 0.5)",
+            data: valores.map((item) => item.amount),
             borderWidth: 1,
-          },
-        ],
+          },{type: 'line',
+            label: 'Gráfico Linha',
+            backgroundColor: "hsl(209, 68%, 66%)",
+            hoverBackgroundColor: "hsl(10, 79%, 65%, 0.5)",
+            data: valores.map((item) => item.amount),
+            borderWidth: 4,
+          }],
+        labels: dias
       },
     });
-    const atualizarBtn = document.getElementById('atualizar');
-    atualizarBtn.addEventListener('click', () => {
-      const valorInput = document.getElementById('valor').value;
-      const diaSelect = document.getElementById('dia').value;
+
+    const form = document.getElementById("form");
+    const valorInput = document.getElementById("valor");
+    const diaSelect = document.getElementById("dia");
+    const atualizarBtn = document.getElementById("atualizar");
+
+    atualizarBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const valor = parseFloat(valorInput.value);
+      const dia = diaSelect.value;
 
       // Encontrar o índice do dia da semana selecionado
-      const diaIndex = valores.findIndex(item => item.day === diaSelect);
+      const diaIndex = valores.findIndex((item) => item.day === dia);
 
-      if(diaIndex !== -1) {
-        //Atualizar o valor no array de valores
-        valores[diaIndex].amount = parseFloat(valorInput);
+      if (diaIndex !== -1) {
+        // Atualizar o valor no array de valores
+        valores[diaIndex].amount = valor;
 
         // Salvar os dados no Armazenamento Local
-        localStorage.setItem('valores', JSON.stringify(valores));
+        localStorage.setItem("valores", JSON.stringify(valores));
 
-        //Atualizza o gráfico
-        chart.data.datasets[0].data = valores.map(item => item.amount);
-        chart.update();
+        // Atualizar o gráfico com os novos dados do gráfico de barra
+        chart.data.datasets[0].data = valores.map((item) => item.amount);
+        // Atualizar o gráfico com os novos dados do gráfico de linha
+        chart.data.datasets[1].data = valores.map((item) => item.amount);
         
+        // Atualizar os gráficos
+        chart.update(); 
+        
+        // Resetar os campos de inputs
+        form.reset();
       }
     });
-  })
-  .catch((erro) => {
-    console.error("Erro ao carregar o arquivo json:", erro);
   });
